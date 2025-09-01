4a7a13e0b4c711994a19656ccc087c4e81abcbf4
Thiruvarankan <thiru07official@gmail.com>
2025-09-02 04:01:37 +0530

    deleted_files

--- aider/dump.py
+++
@@ -1,6 +0,0 @@
-"""Dump utilities for debugging and inspection."""
-
-def dump(*args, **kwargs):
-    """Simple dump function for debugging"""
-    print("Debug dump:", args, kwargs)
-    return args[0] if args else None
