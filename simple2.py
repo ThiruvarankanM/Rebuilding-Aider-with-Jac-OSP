329fc8153192bd4af92b9a83c6c3b5cf44a7418e
ThiruvarankanM <thiru07official@gmail.com>
2025-10-02 11:51:55 +0530

    deleted_files

--- simple2.py
+++
@@ -1,8 +0,0 @@
-from simple1 import User
-
-class Report:
-    def __init__(self):
-        self.user = User("John Doe")
-        
-    def get_status(self):
-        return f"{self.user.name} is {'active' if self.user.active else 'inactive'}"
